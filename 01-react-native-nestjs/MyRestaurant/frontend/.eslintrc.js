a2a59cff3fd3831fa11292115e97df08c1061a9e
Miles <kyw05171@gmail.com>
2024-05-28 17:21:20 +0900

    eslint 및 tsconfig 설정

--- 01-react-native-nestjs/MyRestaurant/frontend/.eslintrc.js
+++ 01-react-native-nestjs/MyRestaurant/frontend/.eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+  "root": true,
+  "env": {
+    "react-native/react-native": true
+  },
+  "extends": [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-native/all",
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "plugins": [
+    "@typescript-eslint",
+    "react",
+    "react-native",
+  ],
+  "rules": {
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      "ignoreRestSiblings": true,
+      "argsIgnorePattern": "^_"
+    }],
+    "@typescript-eslint/semi": "error",
+    "@typescript-eslint/quotes": ["warn", "single"],
+    "@typescript-eslint/indent": ["error", 4, {
+      "SwitchCase": 1,
+      "ignoredNodes": ["TSTypeParameterInstantiation"],
+    }],
+    "@typescript-eslint/no-explicit-any": "off",
+
+    "react-native/no-unused-styles": "off",
+    "react-native/no-inline-styles": "off",
+    "react-native/sort-styles": "off",
+    "react-native/no-color-literals": "off",
+  },
+  "ignorePatterns": [
+    "*.js",
+    "*.json",
+    "*.config.js",
+    ".eslintrc.js"
+  ]
+}
