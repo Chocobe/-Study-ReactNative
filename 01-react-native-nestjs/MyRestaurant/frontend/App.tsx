a68d95bcadbdaf27d920c35a72ebd8a68c9502b0
Miles <kyw05171@gmail.com>
2024-05-28 18:09:06 +0900

    기본 Native 컴포넌트 실습

--- 01-react-native-nestjs/MyRestaurant/frontend/App.tsx
+++ 01-react-native-nestjs/MyRestaurant/frontend/App.tsx
@@ -9,7 +9,11 @@ import React from 'react';
 import type {PropsWithChildren} from 'react';
 import {
     StyleSheet,
+    SafeAreaView,
     View,
+    Text,
+    Button,
+    TextInput,
 } from 'react-native';
 
 function App(): React.JSX.Element {
@@ -19,13 +23,43 @@ function App(): React.JSX.Element {
     }
 
     return (
-        <View>
-        </View>
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Hello React Native
+                </Text>
+            </View>
+
+            <View>
+                <Button
+                    title="Click me 🚀"
+                    onPress={e => {
+                        console.group('onPress()');
+                        console.log('클릭함');
+                        console.groupEnd();
+                    }} />
+
+                <TextInput />
+                <TextInput />
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    //
+    container: {
+        // marginTop: 60,
+        padding: 20,
+        backgroundColor: '#ff1493',
+    },
+
+    title: {
+        paddingVertical: 8,
+        paddingRight: 20,
+        color: '#fff',
+        fontWeight: '700',
+        textAlign: 'right',
+    },
 });
 
 export default App;
