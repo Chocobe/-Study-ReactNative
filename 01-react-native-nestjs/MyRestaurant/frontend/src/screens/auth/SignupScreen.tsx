aeb21aa9f4c1207cf43c42215d692265d0bc6ca3
Miles <kyw05171@gmail.com>
2024-06-03 23:46:15 +0900

    SignupScreen 컴포넌트 구현

--- 01-react-native-nestjs/MyRestaurant/frontend/src/screens/auth/SignupScreen.tsx
+++ 01-react-native-nestjs/MyRestaurant/frontend/src/screens/auth/SignupScreen.tsx
@@ -1,11 +1,15 @@
 import {
     SafeAreaView,
     View,
-    Text,
+    StyleSheet,
 } from 'react-native';
 import { DrawerScreenProps } from '@react-navigation/drawer';
 import { authNavigations } from '../../constants';
 import { AuthStackParamList } from '../../navigations/stack/AuthStackNavigator';
+import MilesInputField from '../../components/MilesInputField';
+import MilesButton from '../../components/MilesButton';
+import useForm from '../../hooks/useForm';
+import { validateSignup } from '../../utils';
 
 type SignupScreenProps = DrawerScreenProps<
     AuthStackParamList,
@@ -13,15 +17,47 @@ type SignupScreenProps = DrawerScreenProps<
 >;
 
 function SignupScreen(_props: SignupScreenProps) {
+    const signupForm = useForm({
+        initialValue: {
+            email: '',
+            password: '',
+            passwordConfirm: '',
+        },
+        validate: validateSignup,
+    });
+
     return (
-        <SafeAreaView>
-            <View>
-                <Text>
-                    Signup Screen
-                </Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <MilesInputField
+                    placeholder="이메일"
+                    inputMode="email"
+                    {...signupForm.getTextInputProps('email')} />
+                <MilesInputField
+                    placeholder="비밀번호"
+                    secureTextEntry
+                    {...signupForm.getTextInputProps('password')} />
+                <MilesInputField
+                    placeholder="비밀번호 확인"
+                    secureTextEntry
+                    {...signupForm.getTextInputProps('passwordConfirm')} />
             </View>
+
+            <MilesButton
+                label="회원가입" />
         </SafeAreaView>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 30,
+    },
+    inputContainer: {
+        gap: 20,
+        marginBottom: 30,
+    },
+});
 
 export default SignupScreen;
--- 01-react-native-nestjs/MyRestaurant/frontend/src/utils/validate.ts
+++ 01-react-native-nestjs/MyRestaurant/frontend/src/utils/validate.ts
@@ -1,29 +1,93 @@
+const EMAIL_REGEXP = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 type UserInformation = {
     email: string;
     password: string;
 };
 
-export const validateLogin = (values: UserInformation) => {
+export const validateUser = (values: UserInformation) => {
     const {
         email,
-        password
+        password,
     } = values;
-
-    const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     const errors = {
         email: '',
         password: '',
     };
 
-    console.log('validateLogin() - values: ', values);
-
-    if (!emailRegExp.test(email)) {
+    if (!EMAIL_REGEXP.test(email)) {
         errors.email = '올바른 이메일 형식이 아닙니다.';
     }
 
     if (password.length < 4 || password.length > 8) {
         errors.password = '비밀번호는 4 ~ 8 자 사이로 입력해주세요.';
+    }
+
+    return errors;
+};
+
+export const validateLogin = (values: UserInformation) => {
+    // const {
+    //     email,
+    //     password
+    // } = values;
+
+    // // const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // const errors = {
+    //     email: '',
+    //     password: '',
+    // };
+
+    // // if (!emailRegExp.test(email)) {
+    // if (!EMAIL_REGEXP.test(email)) {
+    //     errors.email = '올바른 이메일 형식이 아닙니다.';
+    // }
+
+    // if (password.length < 4 || password.length > 8) {
+    //     errors.password = '비밀번호는 4 ~ 8 자 사이로 입력해주세요.';
+    // }
+
+    // return errors;
+    return validateUser(values);
+};
+
+export type SignupInformation = UserInformation & {
+    passwordConfirm: string;
+};
+
+export const validateSignup = (values: SignupInformation) => {
+    const {
+        password,
+        passwordConfirm,
+    } = values;
+
+    // const _errors = {
+    //     email: '',
+    //     password: '',
+    //     passwordConfirm: '',
+    // };
+
+    // if (!EMAIL_REGEXP.test(email)) {
+    //     _errors.email = '올바른 이메일 형식이 아닙니다.';
+    // }
+
+    // if (password.length < 4 || password.length > 8) {
+    //     _errors.password = '비밀번호는 4 ~ 8 자 사이로 입력해주세요.';
+    // }
+
+    // if (password !== passwordConfirm) {
+    //     _errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+    // }
+
+    const errors = {
+        ...validateUser(values),
+        passwordConfirm: '',
+    };
+
+    if (passwordConfirm !== password) {
+        errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
     }
 
     return errors;
